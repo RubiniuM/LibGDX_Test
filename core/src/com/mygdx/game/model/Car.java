7b99b16f5d5c3f464075e4dabfe57c4cfd43db98
RubiniuM <temur.tozhieff@i.ua>
2017-01-07 02:08:25 +0200

    Первый комит

---
+++ core/src/com/mygdx/game/model/Car.java
@@ -0,0 +1,13 @@
+package com.mygdx.game.model;
+
+import com.badlogic.gdx.graphics.Texture;
+
+/**
+ * Created by Темур on 07.01.2017.
+ */
+public class Car extends GameObject {
+
+    public Car(Texture texture, float x, float y, float width, float height) {
+        super(texture, x, y, width, height);
+    }
+}
